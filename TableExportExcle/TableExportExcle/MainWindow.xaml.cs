840f74350acfa32ca443091798b5e3d3c6d8fcc1
hasee <hasee@example.com>
2022-06-02 21:57:32 +0800

    add table_to_excel

--- TableExportExcle/TableExportExcle/MainWindow.xaml.cs
+++ TableExportExcle/TableExportExcle/MainWindow.xaml.cs
@@ -1,22 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NPOI.HSSF;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+﻿using System.Windows;
 using Microsoft.Win32;
 using NPOI.HSSF.UserModel;
 using System.IO;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace TableExportExcle
 {
@@ -31,32 +18,78 @@ namespace TableExportExcle
         }
 
 
-        private void click()
+        private void TableToExcel()
         {
             SaveFileDialog sfd = new SaveFileDialog
-            {
-                Filter = "Excel file(*.xls)|*.xls|*.xlsx",
+            { 
+                Filter = "Excel file (*.xls)|*.xlsx|All files (*.*)|*.*",
                 FilterIndex = 0,
                 RestoreDirectory = true,
                 Title = "Export Excel File"
             };
-            //if (sfd.ShowDialog() != DialogResult.ok)
-            //{
-            //    return;
-            //}
-            string myExcel = sfd.FileName;
-            HSSFWorkbook hwf = new HSSFWorkbook();
-            var sheet = hwf.CreateSheet("Sheet1");
-            FileStream file = new FileStream(myExcel, FileMode.Create);
-            hwf.Write(file);
-            file.Close();
-            //DBhelper.SelectForScalar(TbxSql.Text.Trim(), null);
 
+            sfd.ShowDialog();
+          
+            IWorkbook workbook;
+
+            string fileExt = sfd.FileName.Substring(sfd.FileName.LastIndexOf('.')).ToLower();
+            //string fileExt = System.IO.Path.GetExtension(file).ToLower();
+            switch (fileExt)
+            {
+                case ".xlsx":
+                    workbook = new XSSFWorkbook();
+                    break;
+                case ".xls":
+                    workbook = new HSSFWorkbook();
+                    break;
+                default:
+                    workbook = null;
+                    return;
+            }
+
+            // 从sql读取datatable
+            var dt = DBhelper.ExecuteTable(TbxSql.Text.Trim());
+
+            // 创建sheet页签
+            ISheet sheet = string.IsNullOrEmpty(dt.TableName) ? workbook.CreateSheet("Sheet1") : workbook.CreateSheet(dt.TableName);
+
+            // 创建表头
+            IRow row = sheet.CreateRow(0);
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                ICell cell = row.CreateCell(i);
+                cell.SetCellValue(dt.Columns[i].ColumnName);
+            }
+
+            // 创建数据
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                IRow row1 = sheet.CreateRow(i + 1);
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    ICell cell = row1.CreateCell(j);
+                    cell.SetCellValue(dt.Rows[i][j].ToString());
+                }
+            }
+
+            // 转换成字节数组
+            MemoryStream stream = new MemoryStream();
+            workbook.Write(stream);
+            var buffer = stream.ToArray();
+
+            // 将字节数组写入文件
+            using (FileStream fs = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write))
+            {
+                fs.Write(buffer, 0, buffer.Length);
+                fs.Flush();
+            }
         }
 
         private void BtnOK_Click(object sender, RoutedEventArgs e)
         {
-
+            this.PsbProcess.Value = 0;
+            TableToExcel();
+            this.PsbProcess.Value = 100;
         }
     }
 }

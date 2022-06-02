6cd67bf4bfe999f5756f71448712f82c7e0701d3
hasee <hasee@example.com>
2022-06-02 20:45:51 +0800

    2 commit

---
+++ TableExportExcle/TableExportExcle/DBhelper.cs
@@ -0,0 +1,86 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace TableExportExcle
+{
+    public class DBhelper
+    {
+        private const string _connectionString = @"Data Source=C:\Users\kekef\AppData\Roaming\DBeaverData\workspace6\.metadata\sample-database-sqlite-1\Chinook.db;";
+
+        private static SQLiteConnection Con => new SQLiteConnection(_connectionString);
+
+        private static SQLiteCommand Cmd => Con.CreateCommand();
+
+        public static bool Update(string sql, params SQLiteParameter[] parameters)
+        {
+            var cmd = Cmd;
+            cmd.CommandText = sql;
+            try
+            {
+                if (parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+                return cmd.ExecuteNonQuery() > 0;
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+        }
+      
+        public static object SelectForScalar(string sql, params SQLiteParameter[] parameters)
+        {
+            var cmd = Cmd;
+            cmd.CommandText = sql;
+            try
+            {
+                if (parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+                return cmd.ExecuteScalar();
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+        }
+
+      
+        public static SQLiteDataReader? SelectForDataReader(string sql, params SQLiteParameter[] parameters)
+        {
+            var cmd = Cmd;
+            cmd.CommandText = sql;
+            try
+            {
+                if (parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+                return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            }
+            catch (System.Exception)
+            {
+                cmd.Connection.Close();
+                return null;
+            }
+        }
+
+
+        public static DataTable ExecuteTable(string sql, params SQLiteParameter[] parameters)
+        {
+            var dt = new DataTable();
+            using (var sda = new SQLiteDataAdapter(sql, Con))
+            {
+                if (parameters != null)
+                {
+                    sda.SelectCommand.Parameters.AddRange(parameters);
+                }
+                sda.Fill(dt);
+            }
+            return dt;
+        }
+
+    }
+}
---
+++ TableExportExcle/TableExportExcle/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.HSSF;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+using NPOI.HSSF.UserModel;
+using System.IO;
+
+namespace TableExportExcle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void click()
+        {
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "Excel file(*.xls)|*.xls|*.xlsx",
+                FilterIndex = 0,
+                RestoreDirectory = true,
+                Title = "Export Excel File"
+            };
+            //if (sfd.ShowDialog() != DialogResult.ok)
+            //{
+            //    return;
+            //}
+            string myExcel = sfd.FileName;
+            HSSFWorkbook hwf = new HSSFWorkbook();
+            var sheet = hwf.CreateSheet("Sheet1");
+            FileStream file = new FileStream(myExcel, FileMode.Create);
+            hwf.Write(file);
+            file.Close();
+            //DBhelper.SelectForScalar(TbxSql.Text.Trim(), null);
+
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
